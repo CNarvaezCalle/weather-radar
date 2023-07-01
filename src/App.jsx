7c12e44ecb1d85a71d0c238f75589f4e2d12aeb9
Carlos Narvaez <128408229+CNarvaezCalle@users.noreply.github.com>
2023-07-01 12:04:11 -0500

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import './App.css'
+import Clima from './components/Clima'
 import Weather from './components/Weather'
 
-function App() {
-  
 
-  return (
+
+function App() {
+
+  // function success(position){
+  //   const lati = position.coords;
+  // }
+  
+  // navigator.geolocation.getCurrentPosition(success)
+
+//function coordinadas(posicion) {
+  //const crd = posicion.coords;
+
+//   console.log("Your current position is:");
+//   console.log(`Latitude : ${posicion.coords.latitude}`);
+//   console.log(`Longitude: ${posicion.longitude}`);
+//   console.log(`More or less ${posicion.coords.accuracy} meters.`);
+// }
+
+
+// navigator.geolocation.getCurrentPosition(coordinadas);
+
+   return (
     <>
-    <h1>hola</h1>
-    <Weather />
+    {/* <h1>{navigator.geolocation.getCurrentPosition(coordinadas)}</h1> */}
+    <Weather />   
     </>
   )
 }
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,37 +1,78 @@
-import { useState, useEffect } from "react"
-import axios from 'axios';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 const Weather = () => {
-    const [latitude, setLatitude] = useState('')
-    const [longitude, setLongitude] = useState('')
 
-    const API_endpoint = `https://api.openweathermap.org/data/2.5/weather?`;
-    const API_key = `a3888691b1409d370827f0cfc2b82562`
+    const apiKey = 'a3888691b1409d370827f0cfc2b82562'
+    const [ dataInfo, setDataInfo ] = useState({})
+    const [ units, setUnits ] = useState('metric')
+    const [ isFarenheit, setIsFarenheit ] = useState(false)
+    const [ buttonText, setButtonText ] = useState("Farenheit")
 
     useEffect(() => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            setLatitude(position.coords.latitude);
-            setLongitude(position.coords.longitude)
-        })
+        navigator.geolocation.getCurrentPosition(coords);
+    }, [isFarenheit, buttonText])//termina aqui o solo el geo mas arriba? -- isFarenheit o units??
 
-        // console.log(`${API_endpoint}
-        // lat=${latitude}
-        // &lon=${longitude}
-        // &appid=${API_key}`)
+        function coords(position) {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            // const moreOrLess = `${position.coords.accuracy} meters?`
 
-        let finalAPIEndPoint = `${API_endpoint}lat=${latitude}&lon=${longitude}&appid=${API_key}`
+            
+            
+            axios
+            .get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&lang=es&units=${units}`)
+            .then(resp => {
+                console.log(resp.data)
+                setDataInfo(resp.data)
+            })
+            .catch(err => console.error(err))
+        }
+        
+        const unit = () => {
+            if (isFarenheit) {
+                setIsFarenheit(!isFarenheit)
+                setUnits('metric')
+            } else {
+                setIsFarenheit(!isFarenheit)
+                setUnits('imperial')
+            }
+        }
 
-        axios
-        .get(finalAPIEndPoint)
-        .then((response) => {
-            console.log(response.data)
-        })
+        const button = () => {
+            if (buttonText('Farenheit')) {
+                setButtonText('Celcius')
+            } else {
+                setButtonText('Farenheit')
+            }
+        }
 
-    }, []) 17:13
- 
     return(
-        <h2></h2>
+        <div className="container">
+            <section>//sensacion termica, humedad, presion, temperatura, temp max, temp min // nombre, pais // nubloso, main(clouds)? // velocidad viento</section>
+            {/* CAMBIAR TODO A INGLES DESPUÉS */}
+            <section>
+                <span>Ciudad: {dataInfo.name} </span><span> {dataInfo.sys?.country}</span>
+            </section>
+            <section>
+                <p>Temperatura: {dataInfo.main?.temp}</p>
+                <p>Senación Térmica: {dataInfo.main?.feels_like}</p>
+                <p>Temp. max: {dataInfo.main?.temp_max}</p>
+                <p>Temp. min: {dataInfo.main?.temp_min}</p>
+                <p>Humedad: {dataInfo.main?.humidity}</p>
+                <p>Presión: {dataInfo.main?.pressure}</p>
+            </section>
+            <section>
+                {/* fallo en firefox */}
+                <p>Descripción: {dataInfo.weather && dataInfo.weather[0] && dataInfo.weather[0].description}</p>
+                <p>Nubes: {dataInfo.weather && dataInfo.weather[0] && dataInfo.weather[0]?.main}</p>
+            </section>
+            <section>
+                <p>Viento: {dataInfo.wind?.speed}</p>
+            </section>
+            <button onClick={() => {button(); unit()}}>{buttonText}</button>
+        </div>
     )
 }
 
