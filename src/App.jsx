f49196307b188a025b42f170078d7e243163c670
Carlos Narvaez <128408229+CNarvaezCalle@users.noreply.github.com>
2023-07-02 11:24:18 -0500

    remade-css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import Clima from './components/Clima'
 import Weather from './components/Weather'
 import Header from './components/Header'
 
@@ -7,27 +6,10 @@ import Header from './components/Header'
 
 function App() {
 
-  // function success(position){
-  //   const lati = position.coords;
-  // }
-  
-  // navigator.geolocation.getCurrentPosition(success)
-
-//function coordinadas(posicion) {
-  //const crd = posicion.coords;
-
-//   console.log("Your current position is:");
-//   console.log(`Latitude : ${posicion.coords.latitude}`);
-//   console.log(`Longitude: ${posicion.longitude}`);
-//   console.log(`More or less ${posicion.coords.accuracy} meters.`);
-// }
-
-
-// navigator.geolocation.getCurrentPosition(coordinadas);
 
    return (
     <div className='main__container'>
-      <Header />
+      {/* <Header /> */}
       <Weather />   
     </div>
   )
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,78 +1,99 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Weather = () => {
+  const apiKey = "a3888691b1409d370827f0cfc2b82562";
+  const [dataInfo, setDataInfo] = useState({});
+  const [units, setUnits] = useState("metric");
+  const [isFarenheit, setIsFarenheit] = useState(false);
+  const [buttonText, setButtonText] = useState("Change to F°");
 
-    const apiKey = 'a3888691b1409d370827f0cfc2b82562'
-    const [ dataInfo, setDataInfo ] = useState({})
-    const [ units, setUnits ] = useState('metric')
-    const [ isFarenheit, setIsFarenheit ] = useState(false)
-    const [ buttonText, setButtonText ] = useState("Change to F°")
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(coords);
+  }, [isFarenheit, buttonText]); //termina aqui o solo el geo mas arriba? -- isFarenheit o units??
 
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(coords);
-    }, [isFarenheit, buttonText])//termina aqui o solo el geo mas arriba? -- isFarenheit o units??
+  function coords(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    // const moreOrLess = `${position.coords.accuracy} meters?`
 
-        function coords(position) {
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-            // const moreOrLess = `${position.coords.accuracy} meters?`
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&lang=es&units=${units}`
+      )
+      .then((resp) => {
+        console.log(resp.data);
+        setDataInfo(resp.data);
+      })
+      .catch((err) => console.error(err));
+  }
 
-            
-            
-            axios
-            .get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&lang=es&units=${units}`)
-            .then(resp => {
-                console.log(resp.data)
-                setDataInfo(resp.data)
-            })
-            .catch(err => console.error(err))
-        }
-        
-        const unit = () => {
-            if (isFarenheit) {
-                setIsFarenheit(!isFarenheit)
-                setUnits('metric')
-            } else {
-                setIsFarenheit(!isFarenheit)
-                setUnits('imperial')
-            }
-        }
+  const unit = () => {
+    if (isFarenheit) {
+      setIsFarenheit(!isFarenheit);
+      setUnits("metric");
+    } else {
+      setIsFarenheit(!isFarenheit);
+      setUnits("imperial");
+    }
+  };
 
-        const button = () => {
-            if (buttonText === "Change to F°") {
-                setButtonText('Change to C°')
-            } else {
-                setButtonText('Change to F°')
-            }
-        }
+  const button = () => {
+    if (buttonText === "Change to F°") {
+      setButtonText("Change to C°");
+    } else {
+      setButtonText("Change to F°");
+    }
+  };
 
-    return(
-        <div className="container">
-            {/* CAMBIAR TODO A INGLES DESPUÉS */}
-            <div className='data__container'>
-              <div className="data">   
-                <p className='temp'>{Math.round(dataInfo.main?.temp)}</p>
-                {/* <p className='feels__like'>Senación Térmica: {dataInfo.main?.feels_like}</p> */}
-                {/* <p className='temp__max'>Temp. max: {dataInfo.main?.temp_max}</p> */}
-                {/* <p className='temp__min'>Temp. min: {dataInfo.main?.temp_min}</p> */}
-                {/* <p className='humidity'>Humedad: {dataInfo.main?.humidity}</p> */}
-                  
-                    <p className='wind'>{dataInfo.wind?.speed}</p>
-                    <p className='main'>{dataInfo.weather && dataInfo.weather[0] && dataInfo.weather[0]?.main}</p>
-                    <p className='pressure'>{dataInfo.main?.pressure}</p>
-            
-                {/* fallo en firefox */}
-                <p className='name'>{dataInfo.name}, {dataInfo.sys?.country}</p>
-                <p className='description'>{dataInfo.weather && dataInfo.weather[0] && dataInfo.weather[0].description}</p>
-              </div>
-            </div>
-                
-            <button className='unit__button' onClick={() => {button(); unit()}}>{buttonText}</button>
+  return (
+    <div className="container">
+      <div className="header__container">
+        <div>
+          <h1 className="header__title">Weather app</h1>
+          <button className="header__darkmode">luz</button>
         </div>
-            
-    )
-}
+        <input
+          className="header__search"
+          type="search"
+          placeholder="Search your city"
+        />
+      </div>
+      {/* CAMBIAR TODO A INGLES DESPUÉS */}
+      <div className="data__container">
+        <div className="data">
+          <p className="temp">{Math.round(dataInfo.main?.temp)}</p>
+          <p className="wind">{dataInfo.wind?.speed}</p>
+          <p className="main">
+            {dataInfo.weather &&
+              dataInfo.weather[0] &&
+              dataInfo.weather[0]?.main}
+          </p>
+          <p className="pressure">{dataInfo.main?.pressure}</p>
+          {/* fallo en firefox */}
+          <p className="name">
+            {dataInfo.name}, {dataInfo.sys?.country}
+          </p>
+          <p className="description">
+            {dataInfo.weather &&
+              dataInfo.weather[0] &&
+              dataInfo.weather[0].description}
+          </p>
+        </div>
+        {/* <div className="button"> */}
+          <button
+            className="unit__button"
+            onClick={() => {
+              button();
+              unit();
+            }}
+          >
+            {buttonText}
+          </button>
+        {/* </div> */}
+      </div>
+    </div>
+  );
+};
 
-export default Weather
+export default Weather;
